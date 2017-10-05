d77b3c94e607212925918f83f01a238b839fd687
Lorik Dumani <s4loduma@uni-trier.de>
2017-10-05 12:20:08 +0200

    Prepared Main so it's ready to use to compare all metrics

--- Metrics comparison/src/MainMetricsComparator.java
+++ Metrics comparison/src/MainMetricsComparator.java
@@ -17,59 +17,32 @@ public class MainMetricsComparator {
 
 
         // https://stackoverflow.com/a/13011927
-        String sampleOneRandom = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_1", "files").toString();
-        String sampleOneRandomCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_1", "completed").toString();
-
-        String sampleTwoRandom = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_2", "files").toString();
-        String sampleTwoRandomCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_2", "completed").toString();
-
-        String sampleOneJava = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_Java_17-06_sample_100_1", "files").toString();
-        String sampleOneJavaCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_Java_17-06_sample_100_1", "completed").toString();
-
-        String sampleTwoJava = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_Java_17-06_sample_100_2", "files").toString();
-        String sampleTwoJavaCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_Java_17-06_sample_100_2", "completed").toString();
-
-        String sampleOnePlusRandom = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_1+", "files").toString();
-        String sampleOnePlusRandomCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_1+", "completed").toString();
-
-        String sampleTwoPlusRandom = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_2+", "files").toString();
-        String sampleTwoPlusRandomCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_2+", "completed").toString();
-
-        String sampleMostVersions = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_most_versions", "files").toString();
-        String sampleMostVersionsCompleted = System.getProperty("user.dir") + FileSystems.getDefault().getPath("data", "PostId_VersionCount_SO_17-06_sample_100_most_versions", "completed").toString();
+        final String sampleOneRandom = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_17-06_sample_100_1").toString();
+        final String sampleTwoRandom = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_17-06_sample_100_2").toString();
+        final String sampleOneJava = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_Java_17-06_sample_100_1").toString();
+        final String sampleTwoJava = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_Java_17-06_sample_100_2").toString();
+        final String sampleOnePlusRandom = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_17-06_sample_100_1+").toString();
+        final String sampleTwoPlusRandom = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "Metrics comparison", "testdata", "PostId_VersionCount_SO_17-06_sample_100_2+").toString();
 
 
-        List<String> pathToPostHistories = new ArrayList<>();
-        pathToPostHistories.add(sampleOneRandom);
-        pathToPostHistories.add(sampleTwoRandom);
-        pathToPostHistories.add(sampleOneJava);
-        pathToPostHistories.add(sampleTwoJava);
-        pathToPostHistories.add(sampleOnePlusRandom);
-        pathToPostHistories.add(sampleTwoPlusRandom);
-        pathToPostHistories.add(sampleMostVersions);
+        List<String> pathToAllSamples = new ArrayList<>();
+        pathToAllSamples.add(sampleOneRandom);
+        pathToAllSamples.add(sampleTwoRandom);
+        pathToAllSamples.add(sampleOneJava);
+        pathToAllSamples.add(sampleTwoJava);
+        pathToAllSamples.add(sampleOnePlusRandom);
+        pathToAllSamples.add(sampleTwoPlusRandom);
 
 
-        List<String> pathToAllCompletedCSVs = new ArrayList<>();
-        pathToPostHistories.add(sampleOneRandomCompleted);
-        pathToPostHistories.add(sampleTwoRandomCompleted);
-        pathToPostHistories.add(sampleOneJavaCompleted);
-        pathToPostHistories.add(sampleTwoJavaCompleted);
-        pathToPostHistories.add(sampleOnePlusRandomCompleted);
-        pathToPostHistories.add(sampleTwoPlusRandomCompleted);
-        pathToPostHistories.add(sampleMostVersionsCompleted);
-
-
-        for(int i=0; i<pathToPostHistories.size(); i++) {
+        for(int i=0; i<pathToAllSamples.size(); i++) {
             MetricsComparator metricsComparator = new MetricsComparator(
-                    pathToPostHistories.get(i),
-                    pathToAllCompletedCSVs.get(i));
+                    pathToAllSamples.get(i),
+                    pathToAllSamples.get(i));
 
-            metricsComparator.createStatisticsFiles(pathToPostHistories.get(i));
-
+            metricsComparator.createStatisticsFiles(pathToAllSamples.get(i));
         }
 
         stopWatch.stop();
         System.out.println(stopWatch.getTime() + " milliseconds overall");
     }
 }
-
