5d214920ba934e7122c72bf3287fb1860f6f8202
Lorik Dumani <s4loduma@uni-trier.de>
2017-10-26 16:51:09 +0200

    Little refactorings

---
+++ src/de/unitrier/st/soposthistory/metricscomparison/metricsComparison/MeasuredData.java
@@ -0,0 +1,17 @@
+package de.unitrier.st.soposthistory.metricscomparison.metricsComparison;
+
+class MeasuredData {
+
+    int truePositives_text = 0;
+    int falsePositives_text = 0;
+    int trueNegatives_text = 0;
+    int falseNegatives_text = 0;
+
+    int truePositives_code = 0;
+    int falsePositives_code = 0;
+    int trueNegatives_code = 0;
+    int falseNegatives_code = 0;
+
+    MeasuredData() {
+    }
+}
--- src/de/unitrier/st/soposthistory/metricscomparison/metricsComparison/MetricResult.java
+++ src/de/unitrier/st/soposthistory/metricscomparison/metricsComparison/MetricResult.java
@@ -8,22 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-class MeasuredData {
-
-    int truePositives_text = 0;
-    int falsePositives_text = 0;
-    int trueNegatives_text = 0;
-    int falseNegatives_text = 0;
-
-    int truePositives_code = 0;
-    int falsePositives_code = 0;
-    int trueNegatives_code = 0;
-    int falseNegatives_code = 0;
-
-    MeasuredData() {
-    }
-}
-
 public class MetricResult {
 
     StopWatch stopWatch = new StopWatch();
