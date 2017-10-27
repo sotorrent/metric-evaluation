187c09ce1142ce29b60e570d950e9b97b74dafea
Lorik Dumani <s4loduma@uni-trier.de>
2017-10-27 17:20:16 +0200

    Added method that converts completed files to csv format.
     * Added tests to check conversion

---
+++ src/de/unitrier/st/soposthistory/metricscomparison/util/CompletedCSVConverter.java
@@ -0,0 +1,72 @@
+package de.unitrier.st.soposthistory.metricscomparison.util;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+public class CompletedCSVConverter{
+
+    public static String convertString(File file){
+
+        StringBuilder output = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            boolean firstLine = true;
+
+            String line;
+            while((line = br.readLine()) != null){
+                StringTokenizer tokens = new StringTokenizer(line, ";");
+                int count = 0;
+                while(tokens.hasMoreTokens()){
+                    String token = tokens.nextToken();
+                    count++;
+
+                    if(firstLine) {
+                        token = token.replaceAll("\\s+", "");
+                        token = token.replaceAll("\"", "");
+                        output.append(token);
+
+                        if(count <= 6)
+                            output.append(";");
+
+                    }else if(count <= 6){
+                        token = token.replaceAll("\\s+", "");
+                        token = token.replaceAll("\"", "");
+                        output.append(token).append(";");
+                    }else{
+                        output.append(token.substring(1, token.length()));
+                    }
+                }
+                firstLine = false;
+                output.append("\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
+    public static void convertFiles(String directoryOfCompletedFiles){
+
+        File file = new File(directoryOfCompletedFiles);
+        Pattern pattern = Pattern.compile("completed_" + "[0-9]+" + "\\.csv");
+        File[] allCompletedPostVersionListsInFolder = file.listFiles((dir, name) -> name.matches(pattern.pattern())); // https://stackoverflow.com/questions/4852531/find-files-in-a-folder-using-java
+
+        assert allCompletedPostVersionListsInFolder != null;
+        for (File completedCSV : allCompletedPostVersionListsInFolder) {
+            try {
+                PrintWriter printWriter = new PrintWriter(new FileWriter(completedCSV + "_new", false));
+                String convertedContent = CompletedCSVConverter.convertString(completedCSV);
+                printWriter.write(convertedContent);
+                printWriter.flush();
+                printWriter.close();
+            } catch (IOException e) {
+                System.err.println("Failed to convert file '" + completedCSV.getName() + "'.");
+                System.exit(0);
+            }
+        }
+    }
+}
