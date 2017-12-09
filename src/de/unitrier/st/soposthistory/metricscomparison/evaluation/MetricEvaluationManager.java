dc623f2e936d19f2046efd14e6ab2632c6b9ce70
Sebastian Baltes <research@sbaltes.com>
2017-12-09 14:50:38 +0100

    Add TODO

--- src/de/unitrier/st/soposthistory/metricscomparison/evaluation/MetricEvaluationManager.java
+++ src/de/unitrier/st/soposthistory/metricscomparison/evaluation/MetricEvaluationManager.java
@@ -608,6 +608,9 @@ public class MetricEvaluationManager implements Runnable {
      * Add metrics selected after evaluation, with additional thresholds.
      */
     private static void createSelectedSimilarityMetrics() {
+
+        //TODO: add equals metric as baseline
+
         List<Double> thresholds = Arrays.asList(
                 0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
                      0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2,
